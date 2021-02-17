fb88f71e26e0c409add61eb3516e67a2d619b189
EdonJung <2donny@naver.com>
2021-02-18 03:28:09 +0900

    purgecss plugin  that remove unused css file

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,13 @@ const tailwindcss = require('tailwindcss');
 module.exports = {
     plugins: [
         tailwindcss('./tailwind.js'),
-        require('autoprefixer')
+        require('autoprefixer'),
+        require('@fullhuman/postcss-purgecss')({
+            content: [
+                './src/**/*.js',
+                './public/index.html'
+            ],
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+        })
     ]
 }
