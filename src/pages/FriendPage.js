d4a974e8ea6009169bfd2093c367e27d687fb41f
EdonJung <2donny@naver.com>
2021-02-19 01:31:48 +0900

    friend page framework

---
+++ src/pages/FriendPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Footer_nav from '../components/footer_nav';
+import Layout from '../components/layout';
+import Banner from '../components/banner';
+
+const FriendPage = () => {
+    return (
+        <div className="w-full">
+            <Layout>
+                
+                {/* main */}
+                <section className="px-3">
+
+                </section>
+                
+            </Layout>
+        </div>
+    )
+}
+
+export default FriendPage;
