42862d2ab7391f5aa341986ae2d2aab6b33f2651
EdonJung <2donny@naver.com>
2021-02-19 01:32:37 +0900

    configuration updated

--- postcss.config.js
+++ postcss.config.js
@@ -4,12 +4,12 @@ module.exports = {
     plugins: [
         tailwindcss('./tailwind.js'),
         require('autoprefixer'),
-        require('@fullhuman/postcss-purgecss')({
-            content: [
-                './src/**/*.js',
-                './public/index.html'
-            ],
-            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-        })
+        // require('@fullhuman/postcss-purgecss')({
+        //     content: [
+        //         './src/**/*.js',
+        //         './public/index.html'
+        //     ],
+        //     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+        // })
     ]
 }
