ac77b93a8ef9d586f78c479e0a0bd7e3e1c17556
EdonJung <2donny@naver.com>
2021-02-19 01:30:30 +0900

    Material UI : TextField component

---
+++ src/components/UI/textFieldUI.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+
+const TextFieldUI = ({ label, placeholder, submitted, changeHandler }) => {
+  return (
+    <form onSubmit={(e) => submitted(e)} noValidate autoComplete="off">
+      <TextField 
+        id="filled-basic" 
+        variant="filled"
+        placeholder={placeholder} 
+        label={label}
+        onChange={(e) => changeHandler(e)}
+        autoFocus 
+      />
+    </form>
+  );
+}
+export default TextFieldUI;
