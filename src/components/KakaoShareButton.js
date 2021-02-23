b635fd0f63ecdda49e707da0a0c262ddfb13e416
EdonJung <2donny@naver.com>
2021-02-23 18:28:22 +0900

    prevent second initialization

--- src/components/KakaoShareButton.js
+++ src/components/KakaoShareButton.js
@@ -7,13 +7,10 @@ const KakaoShareButton = () => {
 
   const createKakaoButton = () => {
       const kakao = window.Kakao
-      kakao.init(process.env.REACT_APP_KAKAO_KEY);
-      
       if (!kakao.isInitialized()) { //중복 방지
         // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
         kakao.init(process.env.REACT_APP_KAKAO_KEY)
       }
-      
       kakao.Link.createDefaultButton({
         // Render 부분 id=kakao-link-btn 을 찾아 그부분에 렌더링을 합니다
         container: '#kakao-link-btn',
