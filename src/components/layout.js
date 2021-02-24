fe0f9ccf64827e4b78c816d3ab0827b17494fa0b
EdonJung <2donny@naver.com>
2021-02-24 23:59:10 +0900

    layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import Footer_nav from './footer_nav';
 import Banner from './banner';
 import { Link } from 'react-router-dom';
+import { createKakaoButton } from '../components/KakaoShareButton';
 
-const Layout = ({ children, num, headerNone, footerNone }) => {
+const Layout = ({ children, invitement, num, headerNone, footerNone }) => {
+    const shareBtnClicked = useCallback(() => {
+        createKakaoButton('#kakao-link-btn-second');
+    }, []);
     return (
         <div 
             id="layout"
@@ -12,18 +16,30 @@ const Layout = ({ children, num, headerNone, footerNone }) => {
             {!headerNone ? (
                 <>
                     <nav className="flex flex-row justify-between px-3 py-2">
-                        <Link to="/friend">
+                        <Link to="/my-profile">
                             <img
                                 style={{width: 40, height: 40}}
-                                src="https://2donny.github.io/ykring/yk-logo.png"
+                                src="/yk-logo.png"
+                                alt="yk-logo"
                                 className=" rounded-2xl"
                             />
                         </Link>
                         <div className="flex flex-row items-center">
-                            <Link to="/">
-                                <h1 className="mx-2 font-bold hover:text-blue-400">돌아가기</h1>
+                            <Link to="/my-profile">
+                                <img 
+                                    style={{width: 30, height: 30, margin: '0 10px'}}
+                                    src="/notifications-outline.svg"
+                                    alt="notification"
+                                />
                             </Link>
-                            <h1 className="mx-2 cursor-pointer font-bold hover:text-red-600 hover:font-bold">신고하기</h1>
+                            <Link to="/my-profile">
+                                <img 
+                                    style={{width: 30, height: 30, margin: '0 10px'}}
+                                    src="/Article/ellipsis-vertical-outline.svg"
+                                    alt="notification"
+                                />
+                            </Link>
+                            
                         </div>
                     </nav>
                     {/* Banner */}
@@ -40,6 +56,11 @@ const Layout = ({ children, num, headerNone, footerNone }) => {
                         src="/arrow-up-circle.svg"
                         alt="scroll-top"
                         />
+                </aside>
+            ) : null}
+            {invitement ? (
+                <aside style={{width: '100%', position: 'fixed', bottom: 30, left: 'calc(50% - 159px)'}}>
+                    <button onClick={() => shareBtnClicked()} id="kakao-link-btn-second" className="focus:outline-none" style={{padding: '10px 100px', border: '1px solid black', backgroundColor: '#eee', borderRadius: '10px'}}>연고링 초대장 보내기</button>
                 </aside>
             ) : null}
             <main className="h-full min-h-screen">
