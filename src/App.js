40450e3b6fb0da353f98faa2e57e997a453c954a
EdonJung <2donny@naver.com>
2021-02-25 23:53:30 +0900

    Redirect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Suspense } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Start from './pages/Start';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Intro from './pages/Intro';
 
+const Start = React.lazy(() => import('./pages/Start'));
 const Secure = React.lazy(() => import('./pages/Secure'));
 const Login = React.lazy(() => import('./pages/Login'));
 const SettingCotainer = React.lazy(() => import('./pages/SettingPages/SettingContainer'));
@@ -11,18 +12,19 @@ const MyProfile = React.lazy(() => import('./pages/HomePages/MyProfile'));
 
 const App = () => {
   return (
-    <div>
+    <React.Fragment>
       <Suspense fallback={<div className="h-screen flex flex-row justify-center items-center" ><img style={{width: "200px", height: "200px"}} src="/yk-logo.png" /></div>}>
         <Switch>
-        
-            <Route path="/my-profile" component={MyProfile}/>
-            <Route path="/setting/:questionNum" component={SettingCotainer}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/secure" component={Secure}/>
-            <Route path="/" component={Start} exact/> 
+            <Route path="/my-profile" component={MyProfile} exact/>
+            <Route path="/setting/:questionNum" component={SettingCotainer} exact/>
+            <Route path="/login" component={Login} exact/>
+            <Route path="/secure" component={Secure} exact/>
+            <Route path="/start" component={Start} exact/>
+            <Route path="/" component={Intro} exact/> 
+            <Redirect from="*" to="/"/>
         </Switch>
       </Suspense>
-    </div>
+    </React.Fragment>
   )  
 }
 
