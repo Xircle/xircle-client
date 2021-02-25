0cfef141537b771bbf99947ea770ae6d2d450aee
EdonJung <2donny@naver.com>
2021-02-25 23:55:52 +0900

    headroom

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,31 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState, useEffect } from 'react'
 import Footer_nav from './footer_nav';
 import Banner from './banner';
 import { Link } from 'react-router-dom';
 import { createKakaoButton } from '../components/KakaoShareButton';
+import Headroom from 'react-headroom';
 
-const Layout = ({ children, invitement, num, headerNone, footerNone }) => {
+const Layout = ({ children, isIntro, invitement, num, headerNone, footerNone }) => {
+    const [headerColor, setHeaderColor] = useState('black');
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+    }, []);
     const shareBtnClicked = useCallback(() => {
         createKakaoButton('#kakao-link-btn-second');
     }, []);
+
+    // Head room. scroll에 따라 색깔 변경
+    const handleScroll = useCallback(() => {
+        const screenHeight = window.innerHeight;
+        if(window.scrollY > screenHeight) {
+            setHeaderColor('white');
+        }else
+            setHeaderColor('black')
+    }, []);
+    
     return (
-        <div 
-            id="layout"
-            className="flex flex-col mx-auto bg-white md:w-full"
-        >
+        <div id="layout" className="flex flex-col mx-auto bg-white md:w-full">
             {!headerNone ? (
                 <>
                     <nav className="flex flex-row justify-between px-3 py-2">
@@ -45,9 +58,8 @@ const Layout = ({ children, invitement, num, headerNone, footerNone }) => {
                     {/* Banner */}
                     <Banner />
                 </>
-            ) : (
-                null
-            )}
+            ) : null}
+
             {num === '2' || num === '3' ? (
                 <aside>
                     <img
@@ -58,17 +70,40 @@ const Layout = ({ children, invitement, num, headerNone, footerNone }) => {
                         />
                 </aside>
             ) : null}
+
             {invitement ? (
                 <aside style={{width: '100%', position: 'fixed', bottom: 30, left: 'calc(50% - 159px)'}}>
                     <button onClick={() => shareBtnClicked()} id="kakao-link-btn-second" className="focus:outline-none" style={{padding: '10px 100px', border: '1px solid black', backgroundColor: '#eee', borderRadius: '10px'}}>연고링 초대장 보내기</button>
                 </aside>
             ) : null}
-            <main className="h-full min-h-screen">
+
+            {isIntro ? (
+                <header>
+                    <Headroom
+                        style={{zIndex: 50, overflow: 'hidden', backgroundColor: headerColor}}
+                    >
+                        <div className="flex flex-row items-center justify-between px-5 py-7">
+                            <h3 style={{color: headerColor === 'black' ? 'white' : 'black'}} className="m-0 text-xl font-bold text-white">Xircle.연고링</h3>
+                            <nav>
+                                <Link to="/start">
+                                    <label className="text-black cursor-pointer px-5 py-2 rounded-xl font-bold border-2 bg-white hover:bg-white hover:text-black focus:outline-none">사전신청 하기</label>
+                                </Link>
+                            </nav>
+                        </div>
+                    </Headroom>
+                </header>
+            ) : null}
+
+            <main className="min-h-screen">
                 {children}
             </main>
 
-            {/* footer */}
             <Footer_nav footerNone={footerNone}/>
+
+            {/* footer */}
+            <footer>
+                <div></div>
+            </footer>
         </div>
     )
 }
