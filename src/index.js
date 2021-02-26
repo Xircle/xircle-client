52c53de55797f0d0f67965d690c62229ff24a6b1
EdonJung <2donny@naver.com>
2021-02-27 00:19:48 +0900

    npm-run-all: remove -w

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import './styles/main.css';
 import 'semantic-ui-css/semantic.min.css'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import {Provider} from 'react-redux';
