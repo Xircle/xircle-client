f26becad9e1044942d6392fd5d20451d225eee33
EdonJung <2donny@naver.com>
2021-02-28 19:03:41 +0900

    updated

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -2,15 +2,4 @@ import axios from 'axios';
 
 export const Axios = axios.create({
     baseURL: 'https://ykring.herokuapp.com',
-    withCredentials: false,
 })
-
-export const AxiosForCORS = axios.create({
-    baseURL: 'https://ykring.herokuapp.com',
-    headers:{
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Accept': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-    }  
-})
-
