19206fd7bd6fdfd53cf6a1a52f596f2f8b0e50d2
EdonJung <2donny@naver.com>
2021-03-23 20:03:10 +0900

    40, 50 age added

--- src/components/ageGenerator.js
+++ src/components/ageGenerator.js
@@ -9,8 +9,16 @@ function ageGenerator(age){
         return '30초'
     else if(age >= 34 && age <= 37)
         return '30중'
-    else if(age >= 38)
+    else if(age >= 38 && age < 40)
         return '30후'
+    else if(age >= 40 && age <= 43)
+        return '40초'
+    else if(age >= 44 && age <= 47)
+        return '40중'
+    else if(age >= 48 && age <= 49)
+        return '40후'
+    else
+        return '50대'
 }
 
 export default ageGenerator;
