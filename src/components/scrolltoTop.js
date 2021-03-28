7becfc74a2b67073600253c1d1538abaf9b0749e
EdonJung <2donny@naver.com>
2021-03-28 20:57:02 +0900

    ArticleDetail page scrollTop function

--- src/components/scrolltoTop.js
+++ src/components/scrolltoTop.js
@@ -7,3 +7,20 @@ export const scrolltoTop = (setBtnClicked) => {
       behavior: 'smooth'
     })
 };  
+
+export const scrolltoPostNum = (postNum) => {
+  const rootEl = document.documentElement;
+  let top;
+  if(postNum === 0) // 짝수번째 post인경우
+    top = 0;
+  else if(postNum === 1) {
+    top = 890;
+  }else {
+    top = 890 + 830*(postNum-1); // 890 + 860
+  }
+  console.log(top)
+  rootEl.scrollTo({
+    top,
+    behavior: 'smooth'
+  })
+};  
