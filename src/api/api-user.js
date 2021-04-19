02a50a18a5642e175de8805a0e1b8e37dd32a758
EdonJung <2donny@naver.com>
2021-04-19 22:57:46 +0900

    custom SWR hook

---
+++ src/api/api-user.js
@@ -0,0 +1,16 @@
+import { Axios } from '../axios-instance';
+
+export const SearchFriend = async (url, token) => {
+    const res = await Axios.get(url, {
+        headers: {
+            'access-token': token
+        }
+    });
+    console.log(res);
+    const isSuccess = res.data.success;
+    if(!isSuccess) {
+        if(res.data.code === 458)
+            throw new Error('위치허용을 하셔야 친구검색 기능을 사용하실 수 있습니다!');
+    }
+    return res.data.data;
+}
---
+++ src/data/use-user.js
@@ -0,0 +1,22 @@
+import useSWR, { useSWRInfinite } from 'swr';
+import { SearchFriend } from '../api/api-user';
+
+export function useInfiniteScroll(url, token) {
+    if(!url) throw new Error('url must be required');
+
+    const { data, error, isValidating, size, setSize } = useSWRInfinite((pageIndex, previousPageData) => {
+        if(previousPageData && !previousPageData.length) return null; // reached the end
+        return [url + `page=${pageIndex}`, token];
+    }, SearchFriend);
+
+    const filteredData = data ? [].concat(...data) : []
+    const isReachingEnd = data && data[data.length - 1].length < 10
+    return { 
+        filteredData, 
+        error, 
+        isValidating, 
+        isReachingEnd,
+        size, 
+        setSize 
+    }
+}
