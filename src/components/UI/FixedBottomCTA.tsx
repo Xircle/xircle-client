4662b9d9a71cfa690a6c2ef0c40dd5880ab863ab
EdonJung <2donny@naver.com>
2021-05-12 23:59:51 +0900

    Fixed button

---
+++ src/components/UI/FixedBottomCTA.tsx
@@ -0,0 +1,34 @@
+/** @jsxImportSource @emotion/react */
+import React, { ComponentPropsWithoutRef, forwardRef, Ref } from 'react';
+import Button from './Button';
+import { PortalConsumer } from '../../providers/PortalProviders';
+import { css } from '@emotion/react';
+
+const FixedBottomCTA = forwardRef(function FixedBottomCTA(
+  props: ComponentPropsWithoutRef<typeof Button>,
+  forwardedRef: Ref<HTMLButtonElement>
+) {
+  return (
+    <PortalConsumer>
+      <div
+        css={css`
+          position: fixed;
+          left: 0;
+          bottom: 0;
+          width: 100%;
+        `}
+      >
+        <div
+          css={css`
+            padding: 0 20px 18px;
+            text-align: center;
+          `}
+        >
+          <Button fullWidth={false} {...props} ref={forwardedRef} />
+        </div>
+      </div>
+    </PortalConsumer>
+  );
+});
+
+export default FixedBottomCTA;
