b26712f75154b99ae26ea9fe4ebc8fc7aaa9aedf
EdonJung <2donny@naver.com>
2021-05-16 00:40:38 +0900

    style : top06

---
+++ src/components/Top/Top06.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+import { topStyle } from './styles';
+
+const Top06 = styled.p`
+  ${topStyle}
+  font-weight: normal;
+  color: #A7B0C0;
+  font-size: 14px;
+  line-height: 20px;
+  word-break: keep-all;
+`;
+
+export default Top06;
