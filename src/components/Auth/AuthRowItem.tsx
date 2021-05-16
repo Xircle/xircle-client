5ee5359fdac89429dd2ece05705f30b2d6ad326f
EdonJung <2donny@naver.com>
2021-05-16 21:55:49 +0900

    row item for choosing

--- src/components/Auth/AuthRowItem.tsx
+++ src/components/Auth/AuthRowItem.tsx
@@ -3,26 +3,28 @@ import colors from '../../constants/colors';
 import { css } from '@emotion/react';
 
 interface Props {
-    value: string
+    value: string;
+    clicked: boolean;
+    onClick: (value: string) => void;
 }
 
-export default function AuthRowItem({ value }: Props) {
+export default function AuthRowItem({ clicked, onClick, value }: Props) {
     return (
         <div
             css={css`
                 display: flex;
                 align-items: center;
-                background-color: #fff;
+                background-color: ${clicked ? colors.blue100 : '#fff'};
                 border: 1px solid ${colors.blue500};
+                font-weight: ${clicked && 700};
                 box-sizing: border-box;
                 color: ${colors.blue500};
                 padding: 1.25rem 1.5rem;
                 border-radius: 30px;
                 margin: 15px 0;
-                &:active {
-                    background-color: ${colors.blue100};
-                }   
+                cursor: pointer;
             `}
+            onClick={() => onClick(value)}
         >
             {value}
         </div>
