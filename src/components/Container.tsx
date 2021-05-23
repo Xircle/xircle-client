27a4390acf738de3aa9dbd3ce4d6f80bd28b1d9f
EdonJung <2donny@naver.com>
2021-05-23 22:23:17 +0900

    container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -11,6 +11,8 @@ export default function Container({ children, bgColor }: Props) {
   return (
     <div
       css={css`
+        display: flex;
+        flex-direction: column;
         max-width: 100%;
         width: 100%;
         padding: 0;
@@ -20,7 +22,7 @@ export default function Container({ children, bgColor }: Props) {
     >
       <div
         css={css`
-          background: ${bgColor ? bgColor : colors.background};
+          background: ${bgColor ? bgColor : colors.xircleBg};
         `}
       >
         {children}
